3ac833f15d599a4e4eebe0119c7e4a6e8d209ca8
rg4444 <rihards.gailums@gmail.com>
2018-03-04 11:07:05 +0200

    First commit

---
+++ build/CMakeFiles/oscc-joystick-commander.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/srv/oscc-joystick-commander/oscc/api/src/oscc.c" "/srv/oscc-joystick-commander/build/CMakeFiles/oscc-joystick-commander.dir/oscc/api/src/oscc.c.o"
+  "/srv/oscc-joystick-commander/src/commander.c" "/srv/oscc-joystick-commander/build/CMakeFiles/oscc-joystick-commander.dir/src/commander.c.o"
+  "/srv/oscc-joystick-commander/src/joystick.c" "/srv/oscc-joystick-commander/build/CMakeFiles/oscc-joystick-commander.dir/src/joystick.c.o"
+  "/srv/oscc-joystick-commander/src/main.c" "/srv/oscc-joystick-commander/build/CMakeFiles/oscc-joystick-commander.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "KIA_SOUL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../oscc/api/include"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
